54dd9892ee51f0dfde9e1b781c644ad83fad0e4f
Alireza Javadzadeh M <alirezajm123@gmail.com>
2023-03-10 00:39:47 +0330

    feat: animating cards and navbar buttons onHover + changing port to 3000

--- src/components/Home/Card.tsx
+++ src/components/Home/Card.tsx
@@ -1,23 +1,25 @@
 import { Card as MuiCard, CardContent, CardActions } from '@mui/material'
 import { ICard } from '../../interfaces/interfaces'
+import { motion } from 'framer-motion'
 
 const Card = ({ children, variant, action }: ICard) => {
-  return (
-    <MuiCard
-      variant={variant}
-      sx={{
-        height: '100%',
-        width: 'auto',
-        margin: 'auto',
-        textAlign: 'initial',
-        padding: 0,
-      }}
-      elevation={5}
-    >
-      <CardContent>{children}</CardContent>
-      <CardActions>{action}</CardActions>
-    </MuiCard>
-  )
+	return (
+		<motion.div whileHover={{ scale: 1.05 }}>
+			<MuiCard
+				variant={variant}
+				sx={{
+					height: '100%',
+					width: 'auto',
+					margin: 'auto',
+					textAlign: 'initial',
+					padding: 0,
+				}}
+				elevation={5}>
+				<CardContent>{children}</CardContent>
+				<CardActions>{action}</CardActions>
+			</MuiCard>
+		</motion.div>
+	)
 }
 
 export default Card
