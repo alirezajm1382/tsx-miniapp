78005aa5d6069e6acc7a889079b2579cd6abba3f
Alireza Javadzadeh M <alirezajm123@gmail.com>
2023-03-10 01:32:17 +0330

    fix: added some extra margin to the bottom of gallery page so the last row won't be positioned behind the pagination component

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -13,65 +13,73 @@ import { pageAtom } from '@/jotai/galleryJotai'
 import { motion } from 'framer-motion'
 
 const Gallery: FC = () => {
-  const page = useAtomValue(pageAtom)
-  const { isLoading, isError, data } = useQuery(
-    ['gallery-images', page],
-    () => {
-      return axios.get(
-        `https://jsonplaceholder.typicode.com/photos?_start=${
-          page * 12
-        }&_limit=12`
-      )
-    }
-  )
-  return (
-    <>
-      <Helmet>
-        <title>Mini App | Gallery</title>
-      </Helmet>
-      <Container
-        maxWidth='lg'
-        sx={{
-          marginTop: '1rem',
-          width: {
-            xs: '100%',
-            sm: theme.breakpoints.values.sm,
-            lg: theme.breakpoints.values.lg,
-          },
-        }}
-      >
-        <Grid container spacing={2}>
-          {isLoading && (
-            <Typography variant='h6' component='div'>
-              Loading...
-            </Typography>
-          )}
-          {isError && (
-            <Typography variant='h6' component='div'>
-              An error occurred
-            </Typography>
-          )}
-          {data?.data.map((obj: TImageCard) => (
-            <Grid key={obj.id} xs={12} sm={6} lg={4}>
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <Link
-                  to={`/gallery/${obj.id}`}
-                  style={{ textDecoration: 'none' }}
-                >
-                  <ImageCard image={obj} />
-                </Link>
-              </motion.div>
-            </Grid>
-          ))}
-        </Grid>
-        <Pagination />
-      </Container>
-    </>
-  )
+	const page = useAtomValue(pageAtom)
+	const { isLoading, isError, data } = useQuery(
+		['gallery-images', page],
+		() => {
+			return axios.get(
+				`https://jsonplaceholder.typicode.com/photos?_start=${
+					page * 12
+				}&_limit=12`,
+			)
+		},
+	)
+	return (
+		<>
+			<Helmet>
+				<title>Mini App | Gallery</title>
+			</Helmet>
+			<Container
+				maxWidth='lg'
+				sx={{
+					marginTop: '1rem',
+					marginBottom: '3rem',
+					width: {
+						xs: '100%',
+						sm: theme.breakpoints.values.sm,
+						lg: theme.breakpoints.values.lg,
+					},
+				}}>
+				<Grid
+					container
+					spacing={2}>
+					{isLoading && (
+						<Typography
+							variant='h6'
+							component='div'>
+							Loading...
+						</Typography>
+					)}
+					{isError && (
+						<Typography
+							variant='h6'
+							component='div'>
+							An error occurred
+						</Typography>
+					)}
+					{data?.data.map((obj: TImageCard) => (
+						<Grid
+							key={obj.id}
+							xs={12}
+							sm={6}
+							lg={4}>
+							<motion.div
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}>
+								<Link
+									to={`/gallery/${obj.id}`}
+									style={{ textDecoration: 'none' }}>
+									<ImageCard image={obj} />
+								</Link>
+							</motion.div>
+						</Grid>
+					))}
+				</Grid>
+				<Pagination />
+			</Container>
+		</>
+	)
 }
 
 export default Gallery
