b7e6ef4cd24e572f7c8cb6f2c3719692a2b578f0
Alireza Javadzadeh M <alirezajm123@gmail.com>
2023-03-10 01:29:19 +0330

    feat: todopage reworked. todoindividual comp deleted. todocard is now in action

---
+++ src/components/TodoList/TodoCard.tsx
@@ -0,0 +1,78 @@
+import { FC, useState, MouseEvent } from 'react'
+import { motion } from 'framer-motion'
+import { ITodoIndividual } from '@/interfaces/interfaces'
+import {
+	Card,
+	CardContent,
+	Typography,
+	Menu,
+	MenuItem,
+	CardActions,
+	IconButton,
+} from '@mui/material'
+import { MoreHoriz as ActionsIcon } from '@mui/icons-material'
+import { useFormContext } from 'react-hook-form'
+import { useAtom, useSetAtom } from 'jotai'
+import { idDefaultAtom, notesAtom } from '@/jotai/todoJotai'
+
+const TodoCard: FC<ITodoIndividual> = ({ note }) => {
+	const { setValue } = useFormContext()
+	const setIdDef = useSetAtom(idDefaultAtom)
+	const [notes, setNotes] = useAtom(notesAtom)
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+	const open = Boolean(anchorEl)
+	const handleClose = () => {
+		setAnchorEl(null)
+	}
+	const handleClick = (event: MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget)
+	}
+	return (
+		<>
+			<Card variant='outlined'>
+				<CardContent>
+					<Typography variant='h5'>{note.name}</Typography>
+					<Typography
+						variant='body2'
+						color='text.secondary'>
+						{note.email}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<IconButton onClick={handleClick}>
+						<ActionsIcon />
+					</IconButton>
+				</CardActions>
+			</Card>
+			<Menu
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				sx={{ textAlign: 'center' }}>
+				<Typography
+					variant='caption'
+					color='text.secondary'>
+					#{note.id}
+				</Typography>
+				<MenuItem
+					onClick={() => {
+						setValue('name', note.name)
+						setValue('email', note.email)
+						setValue('id', note.id)
+						setIdDef(note.id)
+						handleClose()
+					}}>
+					Edit
+				</MenuItem>
+				<MenuItem
+					onClick={() =>
+						setNotes(notes.filter((_note) => note.id !== _note.id))
+					}>
+					Delete
+				</MenuItem>
+			</Menu>
+		</>
+	)
+}
+
+export default TodoCard
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,72 +1,61 @@
 import { FC } from 'react'
 import {
-  Container,
-  Divider,
-  Chip,
-  Box,
-  Table,
-  TableHead,
-  TableContainer,
-  TableBody,
-  TableRow,
-  TableCell,
-  Paper,
+	Container,
+	Divider,
+	Chip,
+	Box,
+	Unstable_Grid2 as Grid,
 } from '@mui/material'
 import TodoForm from './TodoForm'
-import TodoIndividual from '@/components/TodoList/TodoIndividual'
 import { useAtomValue } from 'jotai'
 import { notesAtom } from '@/jotai/todoJotai'
 import { Helmet } from 'react-helmet'
 import { FormProvider, useForm } from 'react-hook-form'
+import TodoCard from './TodoCard'
 
 const TodoList: FC = () => {
-  const formMethods = useForm()
-  const notes = useAtomValue(notesAtom)
-  return (
-    <>
-      <Helmet>
-        <title>Mini App | Todo</title>
-      </Helmet>
-      <FormProvider {...formMethods}>
-        <Container maxWidth='md'>
-          <TodoForm />
-          <Divider textAlign='center' component='div'>
-            <Chip
-              label={`Notes (${notes.length})`}
-              color={notes.length === 0 ? `error` : `default`}
-              size='medium'
-              sx={{ fontSize: '1rem', fontWeight: 600 }}
-            />
-          </Divider>
-        </Container>
-        <Box sx={{ margin: '1rem' }}>
-          {notes.length !== 0 && (
-            <TableContainer component={Paper}>
-              <Table aria-label='todo table'>
-                <colgroup>
-                  <col style={{ width: '50%' }} />
-                  <col style={{ width: '40%' }} />
-                  <col style={{ width: '10%' }} />
-                </colgroup>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Email</TableCell>
-                    <TableCell>Actions</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {notes.map((note) => (
-                    <TodoIndividual key={note.id} note={note} />
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )}
-        </Box>
-      </FormProvider>
-    </>
-  )
+	const formMethods = useForm()
+	const notes = useAtomValue(notesAtom)
+	return (
+		<>
+			<Helmet>
+				<title>Mini App | Todo</title>
+			</Helmet>
+			<FormProvider {...formMethods}>
+				<Container maxWidth='md'>
+					<TodoForm />
+					<Divider
+						textAlign='center'
+						component='div'>
+						<Chip
+							label={`Notes (${notes.length})`}
+							color={notes.length === 0 ? `error` : `default`}
+							size='medium'
+							sx={{ fontSize: '1rem', fontWeight: 600 }}
+						/>
+					</Divider>
+				</Container>
+				<Box sx={{ margin: '1rem' }}>
+					{notes.length !== 0 && (
+						<Grid
+							container
+							spacing={2}>
+							{notes.map((note) => (
+								<Grid
+									xs={12}
+									md={6}>
+									<TodoCard
+										key={note.id}
+										note={note}
+									/>
+								</Grid>
+							))}
+						</Grid>
+					)}
+				</Box>
+			</FormProvider>
+		</>
+	)
 }
 
 export default TodoList
